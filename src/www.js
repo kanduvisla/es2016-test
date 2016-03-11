64fc180d7440168bf8797d53d10595b4863f38ef
Giel Berkers <info@gielberkers.com>
2016-03-11 21:32:01 +0100

    Preview in the browser

--- src/www.js
+++ src/www.js
@@ -6,3 +6,35 @@ import { Document } from "./elements/document.js";
 import { CircleArray } from "./elements/circleArray.js";
 import { Generator } from "./generator.js";
 
+// Entry point for www:
+document.addEventListener('DOMContentLoaded', function() {
+    // Create SVG document:
+
+    var configuration = {
+        width: 800,
+        height: 800,
+        children: [
+            {
+                type: 'node',
+                attributes: {
+                    transform: 'translate(400,400)'
+                },
+                children: [
+                    {
+                        type: 'circleArray',
+                        children: [
+                            {
+                                type: 'debugCircle'
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
+
+    var generator = new Generator();
+    var svgDocument = generator.parse(configuration);
+
+    document.getElementById('main').innerHTML = svgDocument.render();
+});
